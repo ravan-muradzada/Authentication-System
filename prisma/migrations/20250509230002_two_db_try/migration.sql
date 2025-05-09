cfbf67e5a22200248544b6db632e3d9d929acb89
Ravan <ravan-muradzada@outlook.com>
2025-05-10 03:07:54 +0400

    Just bullshit

---
+++ prisma/migrations/20250509230002_two_db_try/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Task` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Task" DROP CONSTRAINT "Task_ownerId_fkey";
+
+-- DropTable
+DROP TABLE "Task";
+
+-- DropTable
+DROP TABLE "User";
--- test/auth/signup.test.js
+++ test/auth/signup.test.js
@@ -1,5 +1,2 @@
-import request from 'supertest';
-import redis from '../utils/redis-mock.js'
-import { afterAll, beforeEach, describe, expect, it } from '@jest/globals';
 
 
